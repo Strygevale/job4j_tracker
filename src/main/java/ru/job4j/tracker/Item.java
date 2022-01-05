39f578b755b5078296c64e8bf875087a14d749ee
“Андрей <andrey272242@mail.ru>
2022-01-05 11:36:05 +0600

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,21 @@
 package ru.job4j.tracker;
 
 public class Item {
+
     private int id;
     private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
--- src/main/java/ru/job4j/tracker/oop/Computer.java
+++ src/main/java/ru/job4j/tracker/oop/Computer.java
@@ -8,27 +8,34 @@ public class Computer {
 
     private String cpu;
 
+    public Computer() {
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
 
-    public Computer() {
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
     }
 
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
-        Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
-        Computer comp2 = new Computer();
-        comp2.printInfo();
-        computer.printInfo();
-        comp.printInfo();
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        second.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
     }
 }
+
