bd3f6550baa6bfeea7e6fa38e43eec80a6d8d99d
“Андрей <andrey272242@mail.ru>
2022-01-07 11:02:07 +0600

    0. Что такое инкапсуляция. [#3818]

---
+++ src/main/java/ru/job4j/encapsulation/Account.java
@@ -0,0 +1,12 @@
+package ru.job4j.encapsulation;
+
+public class Account {
+
+    private int balance;
+
+    public void addMoney(int money) {
+        if (money > 0) {
+            balance += money;
+        }
+    }
+}
