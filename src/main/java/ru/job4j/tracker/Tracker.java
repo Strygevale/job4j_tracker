dd8e18f25bacdaee18815d3b37bda5e978d4fb35
“Андрей <andrey272242@mail.ru>
2022-01-08 19:00:12 +0600

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,11 +47,12 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        if (indexOf(id) > -1) {
+        int index = indexOf(id);
+        if (index != -1) {
             item.setId(id);
-            items[indexOf(id)] = item;
+            items[index] = item;
         }
-        return false;
+        return true;
     }
 
     public boolean delete(int id) {
