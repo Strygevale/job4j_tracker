c090aa6d0c7d3ba6c62fc708ce474818dccbba62
“Андрей <andrey272242@mail.ru>
2022-01-10 19:55:54 +0600

    6. Метод замены заявки. Tracker.replace [#211748 #248161]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -48,11 +48,12 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             item.setId(id);
             items[index] = item;
         }
-        return true;
+        return rsl;
     }
 
     public boolean delete(int id) {
