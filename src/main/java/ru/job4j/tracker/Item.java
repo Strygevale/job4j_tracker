f04b94c07d0cc9fcf1e9dead42c9702c6b10e30d
“Андрей <andrey272242@mail.ru>
2022-01-12 20:21:02 +0600

    9. toString [#310018 #248177]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -45,6 +45,9 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" + "id=" + id + ", name='" + name + '\'' + ", created=" + created.format(FORMATTER) + '}';
+        return "Item{" + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
