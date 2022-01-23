464804ea3d0207b1a9a752d92192a5072e9c0beb
“Андрей <andrey272242@mail.ru>
2022-01-23 09:53:49 +0600

    0.4. Пользовательские исключения. [#219367 #253399]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,7 @@
 package ru.job4j.ex;
 
 public class FindEl {
+
     public static int indexOf(String[] value, String el) throws ElementNotFoundException {
         for (int i = 0; i < value.length; i++) {
             if (value[i] == el) {
