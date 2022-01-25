c2dd633fbfc53f6e856657a8d2253ba651113187
“Андрей <andrey272242@mail.ru>
2022-01-25 20:48:06 +0600

    2.1. Тест на ValidateInput [#311887 #253449]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -42,11 +42,14 @@ public class ValidateInputTest {
     @Test
     public void whenValidDigitInput() {
         Output out = new StubOutput();
-        Input in = new StubInput(
-                new String[] {"1", "2", "3"}
-        );
+        String[] answer = new String[] {"1", "2", "3"};
+        Input in = new StubInput(answer);
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected, is(1));
+        int[] selected = new int[answer.length];
+        selected[0] = input.askInt("Enter menu:");
+        selected[1] = input.askInt("Enter menu:");
+        selected[2] = input.askInt("Enter menu:");
+        int[] expected = {1, 2, 3};
+        assertArrayEquals(selected, expected);
     }
 }
