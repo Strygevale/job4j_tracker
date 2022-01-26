c1841bf8ba4be681edad454427871315c70f8584
“Андрей <andrey272242@mail.ru>
2022-01-26 18:09:55 +0600

    1. Singleton [#94606 #254363]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -15,27 +15,4 @@ public final class SingleTracker {
         return tracker;
     }
 
-    public Item add(Item item) {
-        return tracker.add(item);
-    }
-
-    public Item findById(int id) {
-        return null;
-    }
-
-    public Item[] findAll() {
-        return tracker.findAll();
-    }
-
-    public Item[] findByName(String key) {
-        return tracker.findByName(key);
-    }
-
-    public boolean replace(int id, Item item) {
-      return tracker.replace(id, item);
-    }
-
-    public boolean delete(int id) {
-       return tracker.delete(id);
-    }
 }
