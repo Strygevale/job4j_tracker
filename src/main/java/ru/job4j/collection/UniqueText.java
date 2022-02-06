9befda1704b7e7bf89c249e4be9b6d5b20709d02
“Андрей <andrey272242@mail.ru>
2022-02-06 14:35:05 +0600

    2. Идентичные тексты [#212655 #260981]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
@@ -16,6 +15,6 @@ public class UniqueText {
                 return false;
             }
         }
-        return rsl;
+        return true;
     }
 }
