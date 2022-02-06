6e8c49243057882e2f615b37170fc76b2f1d21c5
“Андрей <andrey272242@mail.ru>
2022-02-06 16:02:11 +0600

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,63 @@
+package ru.job4j.bank;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        ArrayList<Account> accounts = new ArrayList<>();
+        if (!users.containsKey(user)) {
+            users.put(user, accounts);
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport); //ищем пользователя по паспорту
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport); //ищем пользователя по паспорту
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            for (Account acc : accounts) {
+                if (acc.getRequisite().equals(requisite)) {
+                    return acc;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account outCome = findByRequisite(srcPassport, srcRequisite);
+        Account inCome = findByRequisite(destPassport, destRequisite);
+        if (outCome != null && inCome != null && outCome.getBalance() >= amount) {
+            outCome.setBalance(outCome.getBalance() - amount);
+            inCome.setBalance(inCome.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+}
