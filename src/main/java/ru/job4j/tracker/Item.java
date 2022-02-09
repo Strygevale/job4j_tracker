3300101d88865794b4320b11de70fb447dee24f1
“Андрей <andrey272242@mail.ru>
2022-02-09 18:11:57 +0600

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -50,4 +50,21 @@ public class Item {
                 + ", created=" + created.format(FORMATTER)
                 + '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, created);
+    }
 }
---
+++ src/test/java/ru/job4j/tracker/SortItemByNameTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SortItemByNameTest {
+
+    @Test
+    public void whenAscSort() {
+        List<Item> items = new ArrayList<>(Arrays.asList(
+                new Item("Bob"),
+                new Item("George"),
+                new Item("Andrey")
+        ));
+        List<Item> expected = new ArrayList<>(Arrays.asList(
+                new Item("Andrey"),
+                new Item("Bob"),
+                new Item("George")
+        ));
+        Collections.sort(items, new ItemAscByName());
+        assertThat(expected, is(items));
+    }
+
+    @Test
+    public void whenDescSort() {
+        List<Item> items = new ArrayList<>(Arrays.asList(
+                new Item("Bob"),
+                new Item("George"),
+                new Item("Andrey")
+
+        ));
+        List<Item> expected = new ArrayList<>(Arrays.asList(
+                new Item("George"),
+                new Item("Bob"),
+                new Item("Andrey")
+        ));
+        Collections.sort(items, new ItemDescByName());
+        assertThat(expected, is(items));
+    }
+}
